ab0aa9781b13e217c5a762ff0b9e986e83c9f0fe
shqwu <79960889+shqwu@users.noreply.github.com>
2023-05-08 14:56:48 -0700

    Update initiate.sh

--- Run_project/initiate.sh
+++ Run_project/initiate.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
-data_ID='11_19'
+home_path=''
 
-mkdir /home/shqwu/Almond_code_git/saved_data/$data_ID
-mkdir /home/shqwu/Almond_code_git/saved_data/$data_ID/Gridmet_ACI
-mkdir /home/shqwu/Almond_code_git/saved_data/$data_ID/Gridmet_nc
-mkdir /home/shqwu/Almond_code_git/saved_data/$data_ID/Gridmet_csv
-mkdir /home/shqwu/Almond_code_git/saved_data/$data_ID/MACA_ACI
-mkdir /home/shqwu/Almond_code_git/saved_data/$data_ID/lasso_model
-mkdir /home/shqwu/Almond_code_git/saved_data/$data_ID/MACA_nc
-mkdir /home/shqwu/Almond_code_git/saved_data/$data_ID/MACA_csv
-mkdir /home/shqwu/Almond_code_git/saved_data/$data_ID/projection
-mkdir /home/shqwu/Almond_code_git/saved_data/$data_ID/aci_contribution
-
-
-sed -i "s/data_ID=.*/data_ID='$data_ID'/g" /home/shqwu/Almond_code_git/Gridmet_ACI.py
-sed -i "s/data_ID=.*/data_ID='$data_ID'/g" /home/shqwu/Almond_code_git/Gridmet_create_nc.py
-sed -i "s/data_ID=.*/data_ID='$data_ID'/g" /home/shqwu/Almond_code_git/Gridmet_create_csv.py
-sed -i "s/data_ID=.*/data_ID='$data_ID'/g" /home/shqwu/Almond_code_git/MACA_ACI.py
-sed -i "s/data_ID=.*/data_ID='$data_ID'/g" /home/shqwu/Almond_code_git/MACA_create_csv.py
-sed -i "s/data_ID=.*/data_ID='$data_ID'/g" /home/shqwu/Almond_code_git/MACA_create_nc.py
-sed -i "s/data_ID=.*/data_ID='$data_ID'/g" /home/shqwu/Almond_code_git/Almond_lasso.py
-sed -i "s/data_ID=.*/data_ID='$data_ID'/g" /home/shqwu/Almond_code_git/MACA_projection.py
-sed -i "s/data_ID=.*/data_ID='$data_ID'/g" /home/shqwu/Almond_code_git/run_MACA_ACI.sh
-sed -i "s/data_ID=.*/data_ID='$data_ID'/g" /home/shqwu/Almond_code_git/aci_contribution.py
+mkdir "$home_path"/intermediate_data/Gridmet_ACI
+mkdir "$home_path"/intermediate_data/Gridmet_nc
+mkdir "$home_path"/intermediate_data/Gridmet_csv
+mkdir "$home_path"/intermediate_data/MACA_ACI
+mkdir "$home_path"/intermediate_data/lasso_model
+mkdir "$home_path"/intermediate_data/MACA_nc
+mkdir "$home_path"/intermediate_data/MACA_csv
+mkdir "$home_path"/output_data/projection
+mkdir "$home_path"/output_data/aci_contribution
