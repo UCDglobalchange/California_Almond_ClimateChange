a8a8ed66ad6de62023803d166f53806e67a7b8eb
shqwu <79960889+shqwu@users.noreply.github.com>
2023-05-08 15:19:23 -0700

    Update initiate.sh

--- Run_project/initiate.sh
+++ Run_project/initiate.sh
@@ -2,13 +2,13 @@
 
 home_path=''
 
-mkdir "$home_path"/intermediate_data/Gridmet_ACI
-mkdir "$home_path"/intermediate_data/Gridmet_nc
-mkdir "$home_path"/intermediate_data/Gridmet_csv
-mkdir "$home_path"/intermediate_data/MACA_ACI
-mkdir "$home_path"/intermediate_data/lasso_model
-mkdir "$home_path"/intermediate_data/MACA_nc
-mkdir "$home_path"/intermediate_data/MACA_csv
-mkdir "$home_path"/output_data/projection
-mkdir "$home_path"/output_data/aci_contribution
-mkdir "$home_path"/output_data/plots
+mkdir $home_path/intermediate_data/Gridmet_ACI
+mkdir $home_path/intermediate_data/Gridmet_nc
+mkdir $home_path/intermediate_data/Gridmet_csv
+mkdir $home_path/intermediate_data/MACA_ACI
+mkdir $home_path/intermediate_data/lasso_model
+mkdir $home_path/intermediate_data/MACA_nc
+mkdir $home_path/intermediate_data/MACA_csv
+mkdir $home_path/output_data/projection
+mkdir $home_path/output_data/aci_contribution
+mkdir $home_path/output_data/plots
