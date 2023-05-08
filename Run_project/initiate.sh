08ecd72add8426747b260858064cf2f9e03ee244
shqwu <79960889+shqwu@users.noreply.github.com>
2023-05-08 15:37:03 -0700

    Update initiate.sh

--- Run_project/initiate.sh
+++ Run_project/initiate.sh
@@ -12,3 +12,11 @@ mkdir $home_path/intermediate_data/MACA_csv
 mkdir $home_path/output_data/projection
 mkdir $home_path/output_data/aci_contribution
 mkdir $home_path/output_data/plots
+
+sed -i "s/home_path=.*/home_path=$home_path/g" $home_path/run_MACA_ACI.sh
+sed -i "s/home_path=.*/home_path=$home_path/g" $home_path/run_MACA_create_nc.sh
+sed -i "s/home_path=.*/home_path=$home_path/g" $home_path/submit_Almond_lasso.sh
+sed -i "s/home_path=.*/home_path=$home_path/g" $home_path/submit_Gridmet_ACI.sh
+sed -i "s/home_path=.*/home_path=$home_path/g" $home_path/submit_MACA_ACI.sh
+sed -i "s/home_path=.*/home_path=$home_path/g" $home_path/submit_MACA_projection.sh
+sed -i "s/home_path=.*/home_path=$home_path/g" $home_path/submit_aci_contribution.sh
