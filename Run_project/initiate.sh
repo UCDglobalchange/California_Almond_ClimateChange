4b8aa91fc1240f3826c573f988cd4c54c61e146a
shqwu <79960889+shqwu@users.noreply.github.com>
2023-05-16 14:03:26 -0700

    Update initiate.sh

--- Run_project/initiate.sh
+++ Run_project/initiate.sh
@@ -2,9 +2,11 @@
 
 home_path=''
 
-mkdir $home_path/intermediate_data/
-mkdir $home_path/input_data/
-mkdir $home_path/output_data/
+mkdir $home_path/intermediate_data
+mkdir $home_path/input_data
+mkdir $home_path/input_data/GridMet
+mkdir $home_path/input_data/MACA
+mkdir $home_path/output_data
 mkdir $home_path/intermediate_data/Gridmet_ACI
 mkdir $home_path/intermediate_data/Gridmet_nc
 mkdir $home_path/intermediate_data/Gridmet_csv
@@ -16,6 +18,7 @@ mkdir $home_path/output_data/projection
 mkdir $home_path/output_data/aci_contribution
 mkdir $home_path/output_data/plots
 
+sed -i "s,home_path=.*,home_path='$home_path',g" $home_path/MACA_ACI.py
 sed -i "s,home_path=.*,home_path=$home_path,g" $home_path/run_MACA_ACI.sh
 sed -i "s,home_path=.*,home_path=$home_path,g" $home_path/run_MACA_create_nc.sh
 sed -i "s,-D.*,-D $home_path,g" $home_path/submit_Almond_lasso.sh
@@ -23,3 +26,4 @@ sed -i "s,-D.*,-D $home_path,g" $home_path/submit_Gridmet_ACI.sh
 sed -i "s,-D.*,-D $home_path,g" $home_path/submit_MACA_ACI.sh
 sed -i "s,-D.*,-D $home_path,g" $home_path/submit_MACA_projection.sh
 sed -i "s,-D.*,-D $home_path,g" $home_path/submit_aci_contribution.sh
+
