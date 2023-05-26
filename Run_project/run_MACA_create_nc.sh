5c11be51564469e8e5aac3d5be13cb2df337c700
shqwu <79960889+shqwu@users.noreply.github.com>
2023-05-26 13:26:12 -0700

    Update run_MACA_create_nc.sh

--- Run_project/run_MACA_create_nc.sh
+++ Run_project/run_MACA_create_nc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 home_path=''
 
-model_name_list=('bcc-csm1-1' 'bcc-csm1-1-m' 'BNU-ESM' 'CanESM2' 'CSIRO-Mk3-6-0' 'GFDL-ESM2G' 'GFDL-ESM2M' 'inmcm4' 'IPSL-CM5A-LR' 'IPSL-CM5A-MR' 'CNRM-CM5' 'HadGEM2-CC365' 'HadGEM2-ES365' 'IPSL-CM5B-LR' 'MIROC5' 'MIROC-ESM' 'MIROC-ESM-CHEM')
+model_name_list=('bcc-csm1-1' 'bcc-csm1-1-m' 'BNU-ESM' 'CanESM2' 'CSIRO-Mk3-6-0' 'GFDL-ESM2G' 'GFDL-ESM2M' 'inmcm4' 'IPSL-CM5A-LR' 'IPSL-CM5A-MR' 'CNRM-CM5' 'HadGEM2-CC365' 'HadGEM2-ES365' 'IPSL-CM5B-LR' 'MIROC5' 'MIROC-ESM' 'MIROC-ESM-CHEM' 'MRI-CGCM3')
 for model in "${model_name_list[@]}"
 do
     sed -i "s/model=.*/model='${model}'/g" $home_path/MACA_create_nc.py
