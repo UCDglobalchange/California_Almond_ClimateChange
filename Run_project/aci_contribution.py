d6679b489293c805c66fcc87ca9cddc9c0aa8a97
shqwu <79960889+shqwu@users.noreply.github.com>
2023-06-02 16:31:06 -0700

    Update aci_contribution.py

--- Run_project/aci_contribution.py
+++ Run_project/aci_contribution.py
@@ -1,8 +1,8 @@
 ##code to  calculate ACI's relative contributions to yield change
 
 
-import math 
-import matplotlib.pyplot as plt 
+import math
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import netCDF4 as nc
@@ -19,7 +19,6 @@ import matplotlib as mpl
 
 
 aci_num = 13
-
 home_path=
 input_path_model = home_path+'/intermediate_data/lasso_model/'
 input_path_ACI = home_path+'/intermediate_data/MACA_csv/'
@@ -39,10 +38,10 @@ for i in range(1,1001):
 for i in range(1,11):
     coef_sum = np.row_stack((coef_sum, locals()['coef'+str(i)]))
 
-model_list = ['bcc-csm1-1-m', 'bcc-csm1-1','BNU-ESM', 'CanESM2', 'CSIRO-Mk3-6-0', 'GFDL-ESM2G', 'GFDL-ESM2M', 'inmcm4', 'IPSL-CM5A-LR', 'IPSL-CM5A-MR','CNRM-CM5', 'HadGEM2-CC365','HadGEM2-ES365', 'IPSL-CM5B-LR', 'MIROC5', 'MIROC-ESM', 'MIROC-ESM-CHEM']
+model_list = ['bcc-csm1-1-m', 'bcc-csm1-1','BNU-ESM', 'CanESM2', 'CSIRO-Mk3-6-0', 'GFDL-ESM2G', 'GFDL-ESM2M', 'inmcm4', 'IPSL-CM5A-LR', 'IPSL-CM5A-MR','CNRM-CM5', 'HadGEM2-CC365','HadGEM2-ES365', 'IPSL-CM5B-LR', 'MIROC5', 'MIROC-ESM', 'MIROC-ESM-CHEM','MRI-CGCM3']
 
 ## obtain aci*coef area-weighted CA
-for model in range(0,17):
+for model in range(0,18):
     locals()['aci_contribution_model_rcp45_'+str(model)] = np.zeros((656,1000,aci_num*2))
     locals()['aci_contribution_model_rcp85_'+str(model)] = np.zeros((656,1000,aci_num*2))
     for trial in range(1,11):
@@ -55,27 +54,27 @@ for model in range(0,17):
                 locals()['aci_contribution_model_rcp45_'+str(model)][i,(trial-1)*100+j,:] = aci_rcp45[i,:]*locals()['coef'+str(trial)][j,0:aci_num*2]
                 locals()['aci_contribution_model_rcp85_'+str(model)][i,(trial-1)*100+j,:] = aci_rcp85[i,:]*locals()['coef'+str(trial)][j,0:aci_num*2]
 area = area_csv[0:41,:]
-for model in range(0,17):
+for model in range(0,18):
     for index in range(0,16):
         for year in range(0,41):
-            locals()['aci_contribution_model_rcp45_'+str(model)][index*41+year,:,:] = locals()['aci_contribution_model_rcp45_'+str(model)][index*41+year,:,:]*area[year,index]/np.sum(area[year])             
+            locals()['aci_contribution_model_rcp45_'+str(model)][index*41+year,:,:] = locals()['aci_contribution_model_rcp45_'+str(model)][index*41+year,:,:]*area[year,index]/np.sum(area[year])
             locals()['aci_contribution_model_rcp85_'+str(model)][index*41+year,:,:] = locals()['aci_contribution_model_rcp85_'+str(model)][index*41+year,:,:]*area[year,index]/np.sum(area[year])
-for model in range(0,17):
+for model in range(0,18):
     locals()['aci_contribution_model_rcp45_sum'+str(model)] = np.zeros((41,1000,aci_num*2))
     locals()['aci_contribution_model_rcp85_sum'+str(model)] = np.zeros((41,1000,aci_num*2))
     for county_id in range(0,16):
         for year in range(0,41):
-            locals()['aci_contribution_model_rcp45_sum'+str(model)][year,:,:] = locals()['aci_contribution_model_rcp45_sum'+str(model)][year,:,:] + np.split(locals()['aci_contribution_model_rcp45_'+str(model)],16,axis=0)[county_id][year,:,:]      
-            locals()['aci_contribution_model_rcp85_sum'+str(model)][year,:,:] = locals()['aci_contribution_model_rcp85_sum'+str(model)][year,:,:] + np.split(locals()['aci_contribution_model_rcp85_'+str(model)],16,axis=0)[county_id][year,:,:]      
-aci_contribution_model_rcp45 = np.zeros((17,41,1000,aci_num*2))
-aci_contribution_model_rcp85 = np.zeros((17,41,1000,aci_num*2))
-for model in range(0,17):
+            locals()['aci_contribution_model_rcp45_sum'+str(model)][year,:,:] = locals()['aci_contribution_model_rcp45_sum'+str(model)][year,:,:] + np.split(locals()['aci_contribution_model_rcp45_'+str(model)],16,axis=0)[county_id][year,:,:]
+            locals()['aci_contribution_model_rcp85_sum'+str(model)][year,:,:] = locals()['aci_contribution_model_rcp85_sum'+str(model)][year,:,:] + np.split(locals()['aci_contribution_model_rcp85_'+str(model)],16,axis=0)[county_id][year,:,:]
+aci_contribution_model_rcp45 = np.zeros((18,41,1000,aci_num*2))
+aci_contribution_model_rcp85 = np.zeros((18,41,1000,aci_num*2))
+for model in range(0,18):
     aci_contribution_model_rcp45[model, :,:,:] = locals()['aci_contribution_model_rcp45_sum'+str(model)]
     aci_contribution_model_rcp85[model, :,:,:] = locals()['aci_contribution_model_rcp85_sum'+str(model)]
 aci_contribution_model_rcp45 = np.mean(aci_contribution_model_rcp45, axis=0)
 aci_contribution_model_rcp85 = np.mean(aci_contribution_model_rcp85, axis=0)
 
-for model in range(0,17):
+for model in range(0,18):
     locals()['aci_contribution_model_rcp45_future_'+str(model)] = np.zeros((1264,1000,aci_num*2))
     locals()['aci_contribution_model_rcp85_future_'+str(model)] = np.zeros((1264,1000,aci_num*2))
     for trial in range(1,11):
@@ -87,20 +86,20 @@ for model in range(0,17):
             for j in range(0,100):
                 locals()['aci_contribution_model_rcp45_future_'+str(model)][i,(trial-1)*100+j,:] = aci_rcp45[i,:]*locals()['coef'+str(trial)][j,0:aci_num*2]
                 locals()['aci_contribution_model_rcp85_future_'+str(model)][i,(trial-1)*100+j,:] = aci_rcp85[i,:]*locals()['coef'+str(trial)][j,0:aci_num*2]
-for model in range(0,17):
+for model in range(0,18):
     for index in range(0,16):
-        locals()['aci_contribution_model_rcp45_future_'+str(model)][index*79:(index+1)*79,:,:] = locals()['aci_contribution_model_rcp45_future_'+str(model)][index*79:(index+1)*79,:,:]*area[-1,index]/np.sum(area[-1])             
+        locals()['aci_contribution_model_rcp45_future_'+str(model)][index*79:(index+1)*79,:,:] = locals()['aci_contribution_model_rcp45_future_'+str(model)][index*79:(index+1)*79,:,:]*area[-1,index]/np.sum(area[-1])
         locals()['aci_contribution_model_rcp85_future_'+str(model)][index*79:(index+1)*79,:,:] = locals()['aci_contribution_model_rcp85_future_'+str(model)][index*79:(index+1)*79,:,:]*area[-1,index]/np.sum(area[-1])
-for model in range(0,17):
+for model in range(0,18):
     locals()['aci_contribution_model_rcp45_sum_future_'+str(model)] = np.zeros((79,1000,aci_num*2))
     locals()['aci_contribution_model_rcp85_sum_future_'+str(model)] = np.zeros((79,1000,aci_num*2))
     for county_id in range(0,16):
         for year in range(0,79):
-            locals()['aci_contribution_model_rcp45_sum_future_'+str(model)][year,:,:] = locals()['aci_contribution_model_rcp45_sum_future_'+str(model)][year,:,:] + np.split(locals()['aci_contribution_model_rcp45_future_'+str(model)],16,axis=0)[county_id][year,:,:]      
-            locals()['aci_contribution_model_rcp85_sum_future_'+str(model)][year,:,:] = locals()['aci_contribution_model_rcp85_sum_future_'+str(model)][year,:,:] + np.split(locals()['aci_contribution_model_rcp85_future_'+str(model)],16,axis=0)[county_id][year,:,:]      
-aci_contribution_model_rcp45_future = np.zeros((17,79,1000,aci_num*2))
-aci_contribution_model_rcp85_future = np.zeros((17,79,1000,aci_num*2))
-for model in range(0,17):
+            locals()['aci_contribution_model_rcp45_sum_future_'+str(model)][year,:,:] = locals()['aci_contribution_model_rcp45_sum_future_'+str(model)][year,:,:] + np.split(locals()['aci_contribution_model_rcp45_future_'+str(model)],16,axis=0)[county_id][year,:,:]
+            locals()['aci_contribution_model_rcp85_sum_future_'+str(model)][year,:,:] = locals()['aci_contribution_model_rcp85_sum_future_'+str(model)][year,:,:] + np.split(locals()['aci_contribution_model_rcp85_future_'+str(model)],16,axis=0)[county_id][year,:,:]
+aci_contribution_model_rcp45_future = np.zeros((18,79,1000,aci_num*2))
+aci_contribution_model_rcp85_future = np.zeros((18,79,1000,aci_num*2))
+for model in range(0,18):
     aci_contribution_model_rcp45_future[model, :,:,:] = locals()['aci_contribution_model_rcp45_sum_future_'+str(model)]
     aci_contribution_model_rcp85_future[model, :,:,:] = locals()['aci_contribution_model_rcp85_sum_future_'+str(model)]
 aci_contribution_model_rcp45_future = np.mean(aci_contribution_model_rcp45_future, axis=0)
@@ -117,12 +116,12 @@ aci_contribution_rcp45_total = aci_contribution_rcp45_total[:,:,0:aci_num]
 aci_contribution_rcp85_total = aci_contribution_rcp85_total[:,:,0:aci_num]
 
 
-aci_contribution_rcp45_county_hist = np.zeros((16,17,41,1000,aci_num))
-aci_contribution_rcp85_county_hist = np.zeros((16,17,41,1000,aci_num))
-aci_contribution_rcp45_county_future = np.zeros((16,17,79,1000,aci_num))
-aci_contribution_rcp85_county_future = np.zeros((16,17,79,1000,aci_num))
+aci_contribution_rcp45_county_hist = np.zeros((16,18,41,1000,aci_num))
+aci_contribution_rcp85_county_hist = np.zeros((16,18,41,1000,aci_num))
+aci_contribution_rcp45_county_future = np.zeros((16,18,79,1000,aci_num))
+aci_contribution_rcp85_county_future = np.zeros((16,18,79,1000,aci_num))
 for county in range(0,16):
-    for model in range(0,17):
+    for model in range(0,18):
         aci_contribution_rcp45_county_hist[county,model,:,:,:] = np.split(locals()['aci_contribution_model_rcp45_'+str(model)],16)[county][:,:,0:aci_num]+np.split(locals()['aci_contribution_model_rcp45_'+str(model)],16)[county][:,:,aci_num:aci_num*2]
         aci_contribution_rcp85_county_hist[county,model,:,:,:] = np.split(locals()['aci_contribution_model_rcp85_'+str(model)],16)[county][:,:,0:aci_num]+np.split(locals()['aci_contribution_model_rcp85_'+str(model)],16)[county][:,:,aci_num:aci_num*2]
         aci_contribution_rcp45_county_future[county,model,:,:,:] = np.split(locals()['aci_contribution_model_rcp45_future_'+str(model)],16)[county][:,:,0:aci_num]+np.split(locals()['aci_contribution_model_rcp45_future_'+str(model)],16)[county][:,:,aci_num:aci_num*2]
@@ -159,6 +158,3 @@ np.save(str(save_path)+'aci_contribution_rcp45_county_2050_change.npy',aci_contr
 np.save(str(save_path)+'aci_contribution_rcp85_county_2050_change.npy',aci_contribution_rcp85_county_2050_change)
 np.save(str(save_path)+'aci_contribution_rcp45_county_2090_change.npy',aci_contribution_rcp45_county_2090_change)
 np.save(str(save_path)+'aci_contribution_rcp85_county_2090_change.npy',aci_contribution_rcp85_county_2090_change)
-
-
-
