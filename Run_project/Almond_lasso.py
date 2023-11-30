0c50cda4c2cfad77d1b9abd28b85a705a47d9ab9
shqwu <79960889+shqwu@users.noreply.github.com>
2023-11-30 14:34:58 -0800

    Update Almond_lasso.py

--- Run_project/Almond_lasso.py
+++ Run_project/Almond_lasso.py
@@ -1,5 +1,4 @@
 ##Code to run LASSO regression model with gridMET-ACI (X) and historical almond yield (Y)
-
 import math
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -21,15 +20,15 @@ import yellowbrick
 from yellowbrick.regressor import cooks_distance
 import sys
 
-home_path=
-save_path = home_path+'/intermediate_data/lasso_model/'
+home_path='/home/shqwu/California_Almond_ClimateChange-main/Run_project'
+save_path = home_path+'/intermediate_data/lasso_model/Frost/'
 input_path_ACI = home_path+'/intermediate_data/Gridmet_csv/'
 input_path_yield = home_path+'/input_data/'
 
 trial=np.int64(sys.argv[1])
-aci_num=13
+aci_num=14
 model_list = ['bcc-csm1-1','bcc-csm1', 'BNU-ESM', 'CanESM2', 'CSIRO-Mk3-6-0', 'GFDL-ESM2G', 'GFDL-ESM2M', 'inmcm4', 'IPSL-CM5A-LR', 'IPSL-CM5A-MR','CNRM-CM5', 'HadGEM2-CC365','HadGEM2-ES365', 'IPSL-CM5B-LR', 'MIROC5', 'MIROC-ESM', 'MIROC-ESM-CHEM']
-X = genfromtxt(input_path_ACI+'Gridmet.csv', delimiter = ',')
+X = genfromtxt(input_path_ACI+'Gridmet_Frost.csv', delimiter = ',')
 Y = genfromtxt(input_path_yield+'almond_yield_1980_2020.csv', delimiter = ',')[:,1:].flatten('F')
 distance = yellowbrick.regressor.influence.cooks_distance(X,Y, show = False).distance_ ##data remove by cook's distance
 threhold = yellowbrick.regressor.influence.cooks_distance(X,Y, show = False).influence_threshold_
@@ -45,7 +44,7 @@ score_train = np.zeros((1))
 times = 0
 while times <=0:
     X_train,X_test,y_train,y_test=train_test_split(X,Y, test_size=0.3)
-    lassocv = LassoCV(alphas=[0,0.5,0.1,0.05,0.01,0.005,0.001,0.0005,0.0001,0.00005,0.00001,0.000005,0.000001,0.0000005], max_iter = np.int64(10e6), fit_intercept=False, random_state = trial)
+    lassocv = LassoCV(alphas=[0,5,1,0.5,0.1,0.05,0.01,0.005,0.001,0.0005,0.0001,0.00005,0.00001,0.000005,0.000001,0.0000005], max_iter = np.int64(10e6), fit_intercept=False, random_state = trial)
     lassocv.fit(X_train, y_train) 
     coef[:] = lassocv.coef_
     score_sum[:] = lassocv.score(X,Y)
@@ -56,4 +55,5 @@ savetxt(str(save_path)+'coef_'+str(trial)+'.csv', coef, delimiter = ',')
 savetxt(str(save_path)+'score_'+str(trial)+'.csv', score_sum, delimiter = ',')
 savetxt(str(save_path)+'score_train_'+str(trial)+'.csv', score_train, delimiter = ',')
 savetxt(str(save_path)+'score_test_'+str(trial)+'.csv', score_test, delimiter = ',')
+
 
