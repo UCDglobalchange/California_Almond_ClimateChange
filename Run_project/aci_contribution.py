a162e36f2fae46f64e649cacf90ecdda416467c2
shqwu <79960889+shqwu@users.noreply.github.com>
2024-02-08 14:45:55 -0800

    Update aci_contribution.py

--- Run_project/aci_contribution.py
+++ Run_project/aci_contribution.py
@@ -1,6 +1,5 @@
 ##code to  calculate ACI's relative contributions to yield change
 
-
 import math 
 import matplotlib.pyplot as plt 
 import pandas as pd
@@ -53,6 +52,9 @@ for model in range(0,18):
             for j in range(0,100):
                 locals()['aci_contribution_model_rcp45_'+str(model)][i,(trial-1)*100+j,:] = aci_rcp45[i,:]*locals()['coef'+str(trial)][j,0:aci_num*2]
                 locals()['aci_contribution_model_rcp85_'+str(model)][i,(trial-1)*100+j,:] = aci_rcp85[i,:]*locals()['coef'+str(trial)][j,0:aci_num*2]
+    locals()['aci_contribution_model_rcp45_copy_'+str(model)] = np.copy(locals()['aci_contribution_model_rcp45_'+str(model)])
+    locals()['aci_contribution_model_rcp85_copy_'+str(model)] = np.copy(locals()['aci_contribution_model_rcp85_'+str(model)])
+
 area = area_csv[0:41,:]
 for model in range(0,18):
     for index in range(0,16):
@@ -86,6 +88,9 @@ for model in range(0,18):
             for j in range(0,100):
                 locals()['aci_contribution_model_rcp45_future_'+str(model)][i,(trial-1)*100+j,:] = aci_rcp45[i,:]*locals()['coef'+str(trial)][j,0:aci_num*2]
                 locals()['aci_contribution_model_rcp85_future_'+str(model)][i,(trial-1)*100+j,:] = aci_rcp85[i,:]*locals()['coef'+str(trial)][j,0:aci_num*2]
+    locals()['aci_contribution_model_rcp45_future_copy_'+str(model)] = np.copy(locals()['aci_contribution_model_rcp45_future_'+str(model)])
+    locals()['aci_contribution_model_rcp85_future_copy_'+str(model)] = np.copy(locals()['aci_contribution_model_rcp85_future_'+str(model)])
+    
 for model in range(0,18):
     for index in range(0,16):
         locals()['aci_contribution_model_rcp45_future_'+str(model)][index*79:(index+1)*79,:,:] = locals()['aci_contribution_model_rcp45_future_'+str(model)][index*79:(index+1)*79,:,:]*area[-1,index]/np.sum(area[-1])             
@@ -107,7 +112,6 @@ aci_contribution_model_rcp85_future = np.mean(aci_contribution_model_rcp85_futur
 
 aci_contribution_rcp45_total = np.row_stack((aci_contribution_model_rcp45, aci_contribution_model_rcp45_future))
 aci_contribution_rcp85_total = np.row_stack((aci_contribution_model_rcp85, aci_contribution_model_rcp85_future))
-
 for i in range(0,aci_num):
     aci_contribution_rcp45_total[:,:,i] = aci_contribution_rcp45_total[:,:,i]+aci_contribution_rcp45_total[:,:,i+aci_num]
     aci_contribution_rcp85_total[:,:,i] = aci_contribution_rcp85_total[:,:,i]+aci_contribution_rcp85_total[:,:,i+aci_num]
@@ -116,26 +120,31 @@ aci_contribution_rcp45_total = aci_contribution_rcp45_total[:,:,0:aci_num]
 aci_contribution_rcp85_total = aci_contribution_rcp85_total[:,:,0:aci_num]
 
 
+
+
+
+
+
 aci_contribution_rcp45_county_hist = np.zeros((16,18,41,1000,aci_num))
 aci_contribution_rcp85_county_hist = np.zeros((16,18,41,1000,aci_num))
 aci_contribution_rcp45_county_future = np.zeros((16,18,79,1000,aci_num))
 aci_contribution_rcp85_county_future = np.zeros((16,18,79,1000,aci_num))
 for county in range(0,16):
     for model in range(0,18):
-        aci_contribution_rcp45_county_hist[county,model,:,:,:] = np.split(locals()['aci_contribution_model_rcp45_'+str(model)],16)[county][:,:,0:aci_num]+np.split(locals()['aci_contribution_model_rcp45_'+str(model)],16)[county][:,:,aci_num:aci_num*2]
-        aci_contribution_rcp85_county_hist[county,model,:,:,:] = np.split(locals()['aci_contribution_model_rcp85_'+str(model)],16)[county][:,:,0:aci_num]+np.split(locals()['aci_contribution_model_rcp85_'+str(model)],16)[county][:,:,aci_num:aci_num*2]
-        aci_contribution_rcp45_county_future[county,model,:,:,:] = np.split(locals()['aci_contribution_model_rcp45_future_'+str(model)],16)[county][:,:,0:aci_num]+np.split(locals()['aci_contribution_model_rcp45_future_'+str(model)],16)[county][:,:,aci_num:aci_num*2]
-        aci_contribution_rcp85_county_future[county,model,:,:,:] = np.split(locals()['aci_contribution_model_rcp85_future_'+str(model)],16)[county][:,:,0:aci_num]+np.split(locals()['aci_contribution_model_rcp85_future_'+str(model)],16)[county][:,:,aci_num:aci_num*2]
+        aci_contribution_rcp45_county_hist[county,model,:,:,:] = np.split(locals()['aci_contribution_model_rcp45_copy_'+str(model)],16)[county][:,:,0:aci_num]+np.split(locals()['aci_contribution_model_rcp45_copy_'+str(model)],16)[county][:,:,aci_num:aci_num*2]
+        aci_contribution_rcp85_county_hist[county,model,:,:,:] = np.split(locals()['aci_contribution_model_rcp85_copy_'+str(model)],16)[county][:,:,0:aci_num]+np.split(locals()['aci_contribution_model_rcp85_copy_'+str(model)],16)[county][:,:,aci_num:aci_num*2]
+        aci_contribution_rcp45_county_future[county,model,:,:,:] = np.split(locals()['aci_contribution_model_rcp45_future_copy_'+str(model)],16)[county][:,:,0:aci_num]+np.split(locals()['aci_contribution_model_rcp45_future_copy_'+str(model)],16)[county][:,:,aci_num:aci_num*2]
+        aci_contribution_rcp85_county_future[county,model,:,:,:] = np.split(locals()['aci_contribution_model_rcp85_future_copy_'+str(model)],16)[county][:,:,0:aci_num]+np.split(locals()['aci_contribution_model_rcp85_future_copy_'+str(model)],16)[county][:,:,aci_num:aci_num*2]
 aci_contribution_rcp45_county = np.concatenate((aci_contribution_rcp45_county_hist, aci_contribution_rcp45_county_future),axis=2)
 aci_contribution_rcp85_county = np.concatenate((aci_contribution_rcp85_county_hist, aci_contribution_rcp85_county_future),axis=2)
 
 aci_contribution_rcp45_county_2050_change =  np.mean((np.mean(aci_contribution_rcp45_county[:,:,60:80,:,:], axis=2)-np.mean(aci_contribution_rcp45_county[:,:,21:41,:,:],axis=2)), axis=1)
-aci_contribution_rcp45_county_2090_change =  np.mean((np.mean(aci_contribution_rcp45_county[:,:,100:120,:,:], axis=2)-np.mean(aci_contribution_rcp45_county[:,:,21:41,:,:],axis=2)), axis=1)
+aci_contribution_rcp45_county_2090_change =  np.mean((np.mean(aci_contribution_rcp45_county[:,:,-20:,:,:], axis=2)-np.mean(aci_contribution_rcp45_county[:,:,21:41,:,:],axis=2)), axis=1)
 aci_contribution_rcp45_county_2050_change_percent = np.zeros((16,1000,aci_num))
 aci_contribution_rcp45_county_2090_change_percent = np.zeros((16,1000,aci_num))
 
 aci_contribution_rcp85_county_2050_change =  np.mean((np.mean(aci_contribution_rcp85_county[:,:,60:80,:,:], axis=2)-np.mean(aci_contribution_rcp85_county[:,:,21:41,:,:],axis=2)), axis=1)
-aci_contribution_rcp85_county_2090_change =  np.mean((np.mean(aci_contribution_rcp85_county[:,:,100:120,:,:], axis=2)-np.mean(aci_contribution_rcp85_county[:,:,21:41,:,:],axis=2)), axis=1)
+aci_contribution_rcp85_county_2090_change =  np.mean((np.mean(aci_contribution_rcp85_county[:,:,-20:,:,:], axis=2)-np.mean(aci_contribution_rcp85_county[:,:,21:41,:,:],axis=2)), axis=1)
 aci_contribution_rcp85_county_2050_change_percent = np.zeros((16,1000,aci_num))
 aci_contribution_rcp85_county_2090_change_percent = np.zeros((16,1000,aci_num))
 
@@ -148,6 +157,8 @@ for county in range(0,16):
 
 np.save(str(save_path)+'aci_contribution_rcp45_total.npy',aci_contribution_rcp45_total)
 np.save(str(save_path)+'aci_contribution_rcp85_total.npy',aci_contribution_rcp85_total)
+np.save(str(save_path)+'aci_contribution_rcp45_county_total.npy',aci_contribution_rcp45_county)
+np.save(str(save_path)+'aci_contribution_rcp85_county_total.npy',aci_contribution_rcp85_county)
 
 np.save(str(save_path)+'aci_contribution_rcp45_county_2050_change_percent.npy',aci_contribution_rcp45_county_2050_change_percent)
 np.save(str(save_path)+'aci_contribution_rcp85_county_2050_change_percent.npy',aci_contribution_rcp85_county_2050_change_percent)
@@ -158,6 +169,10 @@ np.save(str(save_path)+'aci_contribution_rcp45_county_2050_change.npy',aci_contr
 np.save(str(save_path)+'aci_contribution_rcp85_county_2050_change.npy',aci_contribution_rcp85_county_2050_change)
 np.save(str(save_path)+'aci_contribution_rcp45_county_2090_change.npy',aci_contribution_rcp45_county_2090_change)
 np.save(str(save_path)+'aci_contribution_rcp85_county_2090_change.npy',aci_contribution_rcp85_county_2090_change)
+
+
+
+
 
 
 
